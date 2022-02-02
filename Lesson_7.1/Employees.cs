e5bb301bca619334915d650d0eb8ba4cb768063f
Eugene <comradeeugene@gmail.com>
2022-02-02 12:58:08 +0300

    bez sortirovki

--- Lesson_7.1/Employees.cs
+++ Lesson_7.1/Employees.cs
@@ -4,196 +4,300 @@ using static System.Console;
 
 namespace Lesson_7
 {
-    struct Employees
-    {
-        Employee[] list;
-        int size;
+	struct Employees
+	{
+		Employee[] list;
+		int size;
  
-        public int Count { get; set; }
-        public int Size
-        {
-            get { return size;
-        }
-            set
-            {
-                if (value < 0)
-                {
-                    size = 0;
-                }
-                else
-                {
-                    size = value;
-                }
-            }
-        }
+		public int Count { get; set; }
+		public int Size
+		{
+			get { return size;
+		}
+			set
+			{
+				if (value < 0)
+				{
+					size = 0;
+				}
+				else
+				{
+					size = value;
+				}
+			}
+		}
 
-        internal Employee[] List { get => list; set => list = value; }
+		internal Employee[] List { get => list; set => list = value; }
 
-        public Employees(int size) : this()                                     
-        {
-            Size = size;
-            Count = 0;
-            list = new Employee[Size];
-        }
+		public Employees(int size) : this()                                     
+		{
+			Size = size;
+			Count = 0;
+			list = new Employee[Size];
+		}
  
-        public Employee this[int index]                                         //индексатор
-        {
-            get { return List[index]; }
-            set { List[index] = value; }
-        }
+		public Employee this[int index]                                         //индексатор
+		{
+			get { return List[index]; }
+			set { List[index] = value; }
+		}
 
-        public void LoadText(string path)                                       //загрузка из файла
-        {
-            if (File.Exists(path))
-            {
-                using (StreamReader sr = new StreamReader(path))
-                {
-                    while (!sr.EndOfStream)
-                    {
-                        if (Count >= list.Length)
-                            Array.Resize(ref list, list.Length * 2);
+		public void LoadText(string path)                                       //загрузка из файла
+		{
+			if (File.Exists(path))
+			{
+				using (StreamReader sr = new StreamReader(path))
+				{
+					while (!sr.EndOfStream)
+					{
+						if (Count >= list.Length)
+							Array.Resize(ref list, list.Length * 2);
 
-                        string[] arr = sr.ReadLine().Split('#');
-                        list[Count] = new Employee(uint.Parse(arr[0]),
-                            DateTime.Parse(arr[1]), arr[2], uint.Parse(arr[3]),
-                            uint.Parse(arr[4]), DateTime.Parse(arr[5]), arr[6]);
+						string[] arr = sr.ReadLine().Split('#');
+						list[Count] = new Employee(uint.Parse(arr[0]),
+							DateTime.Parse(arr[1]), arr[2], uint.Parse(arr[3]),
+							uint.Parse(arr[4]), DateTime.Parse(arr[5]), arr[6]);
 
-                        list[Count].Flag = 1;
-                        ++Count;
-                    }
-                }
-            }
-        }
+						list[Count].Flag = 1;
+						++Count;
+					}
+				}
+			}
+		}
 
-        void ReWrite()                                                          //перезапись 
-        {
-            string str = string.Empty;
-            File.Delete(@"TestList");
-            Count = 0;
-            for (int i = 0; i < list.Length; i++)
-            {
-                if (list[i].Flag == 1)
-	            {
-                    str = $"{list[i].Id}#{list[i].CreatDate}#{list[i].FullName}#" +
-                        $"{list[i].Age}#{list[i].Height}#{list[i].Birthbay}#" +
-                        $"{list[i].BirthPlace}\n";
-                    File.AppendAllText(@"TestList", str);
-                    ++Count;
-	            }
-            }
-        }
+		void ReWrite()                                                          //перезапись 
+		{
+			string str = string.Empty;
+			File.Delete(@"TestList");
+			for (int i = 0; i < list.Length; i++)
+			{
+				if (list[i].Flag == 1)
+				{
+					str = $"{list[i].Id}#{list[i].CreatDate}#{list[i].FullName}#" +
+						$"{list[i].Age}#{list[i].Height}#{list[i].Birthbay}#" +
+						$"{list[i].BirthPlace}\n";
+					File.AppendAllText(@"TestList", str);
+				}
+			}
+		}
  
-        public void AddElement()                                                // Метод добавления элемента
-        {
-            if (Count >= list.Length)
-            {
-                Array.Resize(ref list, list.Length * 2);
-            }
-            string str = string.Empty; 
-            List[Count] = new Employee();
-            List[Count].Flag = 1;
-            List[Count].CreatDate = DateTime.Today;
+		public void AddElement()                                                // Метод добавления элемента
+		{
+			Clear();
+			if (Count >= list.Length)
+			{
+				Array.Resize(ref list, list.Length * 2);
+			}
+			string str = string.Empty; 
+			List[Count] = new Employee();
+			List[Count].Flag = 1;
+			List[Count].CreatDate = DateTime.Today;
 
-            WriteLine("ID");
-            list[Count].Id = CheckParam();
-            str += list[Count].Id + "#" + list[Count].CreatDate + "#";
+			WriteLine("ID");
+			list[Count].Id = CheckParam();
+			str += list[Count].Id + "#" + list[Count].CreatDate + "#";
 
-            WriteLine("Ф.И.О.");
-            list[Count].FullName = ReadLine();
-            str += list[Count].FullName + "#";
+			WriteLine("Ф.И.О.");
+			list[Count].FullName = ReadLine();
+			str += list[Count].FullName + "#";
 
-            WriteLine("Возраст");
-            list[Count].Age = CheckParam();
-            str += list[Count].Age + "#";
+			WriteLine("Возраст");
+			list[Count].Age = CheckParam();
+			str += list[Count].Age + "#";
 
-            WriteLine("Рост");
-            list[Count].Height = CheckParam();
-            str += list[Count].Height + "#";
+			WriteLine("Рост");
+			list[Count].Height = CheckParam();
+			str += list[Count].Height + "#";
 
-            WriteLine("Дата рождения в формате дд.мм.гггг");
-            list[Count].Birthbay = CheckData();
-            str += list[Count].Birthbay.ToString("dd.MM.yyyy") + "#";
+			WriteLine("Дата рождения в формате дд.мм.гггг");
+			list[Count].Birthbay = CheckData();
+			str += list[Count].Birthbay.ToString("dd.MM.yyyy") + "#";
 
-            WriteLine("Место рождения");
-            list[Count].BirthPlace = ReadLine();
-            str += list[Count].BirthPlace + "\n";
+			WriteLine("Место рождения");
+			list[Count].BirthPlace = ReadLine();
+			str += list[Count].BirthPlace + "\n";
 
-            File.AppendAllText(@"TestList", str);
-            ++Count;
-        }
+			File.AppendAllText(@"TestList", str);
+			++Count;
+		}
 
-        public void Print()                                                     //печать списка
-        {
-            for (int i = 0; i < list.Length; i++)
-            {
-                if (list[i].Flag == 1)
-                {
-                    list[i].Print();
-                }
-            }
-        }
+		public void Print()                                                     //печать списка
+		{
+			Clear();
+			for (int i = 0; i < list.Length; i++)
+			{
+				if (list[i].Flag == 1)
+				{
+					list[i].Print();
+				}
+			}
+			ReadKey();
+		}
 
-        public void FindEmployee()                                              //поиск элемента
-        {
-            WriteLine("Введите ID(положительное число)");
-            uint index = CheckParam();
-            for (int i = 0; i < list.Length; i++)
-            {
-                if (list[i].Id == index)
-                {
-                    list[i].Print();
-                    return;
-                }
-            }
-            WriteLine("Запись с таким   ID не существует");
-        }
+		public void FindEmployee()                                              //поиск элемента
+		{
+			Clear();
+			WriteLine("Введите ID(положительное число)");
+			uint index = CheckParam();
+			for (int i = 0; i < list.Length; i++)
+			{
+				if (list[i].Id == index)
+				{
+					Clear();
+					list[i].Print();
+					ReadKey();
+					return;
+				}
+			}
+			Clear();
+			WriteLine("Запись с таким   ID не существует");
+			ReadKey();
+		}
 
-        public void DelEmployee()                                               //удаление элемента
-        {
-            WriteLine("Введите ID(положительное число) удаляемого элемента");
-            uint index = CheckParam();
-            for (int i = 0; i < list.Length; i++)
-            {
-                if (list[i].Id == index)
-                {
-                    Array.Clear(list, i, 1);
-                    ReWrite();
-                    return;
-                }
-            }
-            WriteLine("Запись с таким   ID не существует");
-        }
+		public void ChangeElement()												//редактировать элемент
+		{
+			Clear();
+			WriteLine("Введите ID(положительное число)");
+			uint index = CheckParam();
+			for (int i = 0; i < list.Length; i++)
+			{
+				if (list[i].Id == index)
+				{
+					WriteLine("ID");
+					list[i].Id = CheckParam();
+
+					WriteLine("Ф.И.О.");
+					list[i].FullName = ReadLine();
+
+					WriteLine("Возраст");
+					list[i].Age = CheckParam();
+
+					WriteLine("Рост");
+					list[i].Height = CheckParam();
+
+					WriteLine("Дата рождения в формате дд.мм.гггг");
+					list[i].Birthbay = CheckData();
+
+					WriteLine("Место рождения");
+					list[i].BirthPlace = ReadLine();
+					ReWrite();
+					return;
+				}
+			}
+			Clear();
+			WriteLine("Запись с таким   ID не существует");
+			ReadKey();
+		}
+
+		public void DelEmployee()                                               //удаление элемента
+		{
+			Clear();
+			WriteLine("Введите ID(положительное число) удаляемого элемента");
+			uint index = CheckParam();
+			for (int i = 0; i < list.Length; i++)
+			{
+				if (list[i].Id == index)
+				{
+					Array.Clear(list, i, 1);
+					ReWrite();
+					return;
+				}
+			}
+			Clear();
+			WriteLine("Запись с таким   ID не существует");
+			ReadKey();
+		}
+
+		public void DateRange()													//поиск по диапозону дат
+		{ 
+			Clear();
+			WriteLine("Введите диапозон дат");
+			WriteLine("дат 1:");
+			DateTime date1 = CheckData();
+			WriteLine("дат 2:");
+			DateTime date2 = CheckData();
+			for (int i = 0; i < list.Length; i++)
+			{
+				if (date1 <= list[i].CreatDate && date2 >= list[i].CreatDate)
+				{
+					list[i].Print();
+				}
+			}
+			ReadKey();
+		}
+
+		public void SortEmloyees()												//сортировки
+		{ 
+			WriteLine("1 по возрастанию\n2 по убыванию");
+			string str = ReadLine();
+			Employee temp = new Employee();
+			bool isSorted = false;
+			if (str == "1")
+			{ 
+				while (isSorted)
+				{ 
+					isSorted = true;
+					for (int i = 1; i < list.Length; i++)
+					{
+						if (list[i].Id < list[i - 1].Id) 
+						{
+							temp = list[i];
+							list[i] = list[i -1];
+							list[i - 1] = temp;
+							isSorted = false;
+						}
+					}
+				}
+			}
+			else if (str == "2")
+			{
+				while (isSorted)
+				{ 
+					isSorted = true;
+					for (int i = 1; i < list.Length; i++)
+					{
+						if (list[i].Id > list[i - 1].Id) 
+						{
+							temp = list[i];
+							list[i] = list[i -1];
+							list[i - 1] = temp;
+							isSorted = false;
+						}
+					}
+				}
+			}
+		}
  
-        uint CheckParam()                                                       //проверка параметров
-        {
-            uint param;
-            while (true)
-            {
-                if (uint.TryParse(ReadLine(), out param))
-                {
-                    return param;
-                }
-                else
-                {
-                    WriteLine("Не корректное значение");
-                }
-            }
-        }
+		uint CheckParam()                                                       //проверка параметров
+		{
+			uint param;
+			while (true)
+			{
+				if (uint.TryParse(ReadLine(), out param))
+				{
+					return param;
+				}
+				else
+				{
+					WriteLine("Не корректное значение");
+				}
+			}
+		}
 
-        DateTime CheckData()                                                    //проверка даты
-        {
-            DateTime date;
-            while (true)
-            {
-                if (DateTime.TryParse(ReadLine(), out date))
-                {
-                    return date;
-                }
-                else
-                {
-                    WriteLine("Не корректное значение");
-                }
-            }
-        }
-    }
+		DateTime CheckData()                                                    //проверка даты
+		{
+			DateTime date;
+			while (true)
+			{
+				if (DateTime.TryParse(ReadLine(), out date))
+				{
+					return date;
+				}
+				else
+				{
+					WriteLine("Не корректное значение");
+				}
+			}
+		}
+	}
 }
--- Lesson_7.1/Program.cs
+++ Lesson_7.1/Program.cs
@@ -3,29 +3,51 @@ using static System.Console;
 
 namespace Lesson_7
 {
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Employees emp = new Employees(10);
-            emp.LoadText(@"TestList");
-            do
-            {
-                WriteLine("1 для вывода данных на экран\n2 для " +
-                            "добавления новой записи\n3 для выхода");
-                string Str = ReadLine();
-                switch (Str)
-                {
-                    case "1":
-                        emp.Print();
-                        break;
-                    case "2":
-                        emp.AddElement();
-                        break;
-                    default:
-                        return;
-                }
-            } while (true);
-        }
-    }
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Employees emp = new Employees(10);
+			emp.LoadText(@"TestList");
+			do
+			{
+				Clear();
+				WriteLine("1 вывод данных на экран\n" +
+						  "2 добавить новую записи\n"+
+						  "3 найти запись\n" +
+						  "4 удалить запись\n" +
+						  "5 редактирование запись\n" +
+						  "6 поиск по диапозону дат\n" +
+						  "7 сортировка по дате создания\n" +
+						  "q для выхода");
+				string Str = ReadLine();
+				switch (Str)
+				{
+					case "1":
+						emp.Print();
+						break;
+					case "2":
+						emp.AddElement();
+						break;
+					case "3":
+						emp.FindEmployee();
+						break;
+					case "4":
+						emp.DelEmployee();
+						break;
+					case "5":
+						emp.ChangeElement();
+						break;
+					case "6":
+						emp.DateRange();
+						break;
+					case "7":
+						emp.SortEmloyees();
+						break;
+					default:
+						return;
+				}
+			} while (true);
+		}
+	}
 }
